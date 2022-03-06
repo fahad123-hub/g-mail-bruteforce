a853d36c159ed5acf0c58e3c4ccd6be9b3a4a598
RED HACKERS ARMY <68217984+fahad123-hub@users.noreply.github.com>
2022-03-06 15:24:21 +0300

    gmail-4.py

--- gmail-4.py
+++ gmail-4.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
+import time #line:3
+from colorama import Fore ,Back ,Style ,init #line:4
+init (autoreset =True )#line:5
+def startMessage ():#line:7
+    OO0O0OO0OOO0OO0O0 =input (Fore .YELLOW +"Enter Code To Unlock The Tool : ")#line:8
+    OOOO0OO000OO0OOOO ="@hackerexploits"#line:9
+    if OOOO0OO000OO0OOOO !=OO0O0OO0OOO0OO0O0 :#line:10
+        print (Fore .RED +'[X] Wrong Code')#line:11
+        print (Fore .BLUE +''' 
 from __future__ import absolute_import
 from __future__ import print_function
 from six.moves import input
